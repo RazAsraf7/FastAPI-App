a2da5140c3967915fc1559a743b2fadb65b5bed2
Raz Asraf <razasraf7@gmail.com>
2024-06-30 12:58:06 +0300

    Application is ready to deploy!

--- DMD.py
+++ DMD.py
@@ -4,6 +4,7 @@ from fastapi.templating import Jinja2Templates
 from fastapi.logger import logger
 from pymongo import MongoClient, ReturnDocument
 from bson import ObjectId
+from random import randint
 import json
 import uvicorn
 import re
@@ -105,7 +106,6 @@ async def register(
 
         # Insert the new user into the users collection
         users_collection.insert_one(user_data)
-        hobbies_collection.insert_one({"username":User_Name})
 
         # Redirect to the success page with user details in query parameters
         return RedirectResponse(url=f'/register_success?User_Name={User_Name}&Full_Name={First_Name} {Last_Name}&Email={Email}&City={City}&Gender={Gender}', status_code=302)
@@ -162,6 +162,17 @@ async def logout(request: Request):
     response.delete_cookie("user_name")
     return response
 
+def get_users_by_hobby(hobby):
+    pipeline = [
+        {"$match": {"hobby": hobby}},
+        {"$project": {"_id": 0, "username": 1}}
+    ]
+
+    result = db.hobbies.aggregate(pipeline)
+    users = list(result)
+    return users
+
+
 def retrieve_user_id(user_name):
     user = users_collection.find_one({"username": user_name})
     if user:
@@ -175,6 +186,60 @@ def retrieve_username(user_id):
         return user['username']
     else:
         return None
+
+def get_firstname_by_username(username):
+    user = users_collection.find_one({"username": username})
+    if user:
+        return user['firstname']
+    else:
+        return 'Username not found'
+    
+
+def get_all_usernames():
+    pipeline = [
+        {"$project": {"_id": 0, "username": 1}}
+    ]
+
+    result = db.users.aggregate(pipeline)
+    usernames = list(result)
+    usernames_json = json.dumps(usernames)
+
+    return usernames_json
+
+@app.get('/api/hobbies/get_all_firstnames_and_hobbies')
+async def get_all_firstnames_and_hobbies():
+    users = []
+    user_hobby = []
+    firstname_hobby = []
+    ready_list = []
+    all_users_dict = json.loads(get_all_usernames())
+    for user in all_users_dict:
+        if user != 'admin':
+            users.append(user['username'])
+    result = hobbies_collection.aggregate([{'$project': {'_id': 0, 'username': 1, 'hobby': 1}}])
+    for data in result:
+        user_hobby.append(data)
+    for data in user_hobby:
+        firstname_hobby.append({get_firstname_by_username(data['username']): data['hobby']})
+    for data in firstname_hobby:
+        for firstname, hobby in data.items():
+            ready_list.append(f'{firstname} likes {hobby}!')
+    return ready_list
+
+
+@app.get('/api/hobbies/get_hobbies_by_username/{username}', response_class=HTMLResponse)
+def get_hobbies_by_username(username):
+    pipeline = [
+        {"$match": {"username": username}},
+        {"$project": {"_id": 0, "hobby": 1}}
+    ]
+
+    result = db.hobbies.aggregate(pipeline)
+    
+    # Extract data from the CommandCursor
+    hobbies_data = [data for data in result]
+    
+    return hobbies_data   
     
 @app.get('/api/hobbies/{username}', response_class=HTMLResponse)
 def get_user_hobbies(username):
@@ -189,24 +254,73 @@ def get_user_hobbies(username):
 
     return hobbies_json
 
-# @app.get('/api/user_details/{username}', response_class=HTMLResponse)
-# async def get_user_details(request: Request, username: str):
-#     user = users_collection.find_one({"username": username})
-#     if user:
-#         user_details = {
-#             "username": user.get("username"),
-#             "email": user.get("email"),
-#             "first_name": user.get("firstname"),
-#             "last_name": user.get("lastname"),
-#             "phone_number": user.get("phone_number"),
-#             "full_address": user.get("full_address")
-#         }
-#         return JSONResponse(content=json.dumps(user_details))
-#     else:
-#         raise HTTPException(status_code=404, detail="User not found")
+@app.get('/api/user_details/{username}')
+async def get_user_details(request: Request, username: str):
+    user = users_collection.find_one({"username": username})
+    if user:
+        user_details = {
+            "username": user.get("username"),
+            "email": user.get("email"),
+            "first_name": user.get("firstname"),
+            "last_name": user.get("lastname"),
+            'full_name': user.get('full_name'),
+            "phone_number": user.get("phone_number"),
+            "full_address": user.get("full_address")
+        }
+        return user_details
+    else:
+        raise HTTPException(status_code=404, detail="User not found")
+
+
+@app.get('/api/users/find_shared_hobbies/{username}')
+async def find_shared_hobbies(username: str):
+    # Fetch all users except admin and the current user
+    all_users = list(users_collection.find({"username": {"$ne": "admin", "$ne": username}}, {"_id": 0, "username": 1}))
+    
+    # Fetch hobbies of the requesting user
+    my_hobbies = [hobby['hobby'] for hobby in hobbies_collection.find({"username": username}, {"_id": 0, "hobby": 1})]
+    
+    matches = []
+
+    # For each user, fetch their hobbies and find matches
+    for user in all_users:
+        user_hobbies = [hobby['hobby'] for hobby in hobbies_collection.find({"username": user['username']}, {"_id": 0, "hobby": 1})]
+        common_hobbies = set(my_hobbies).intersection(user_hobbies)
+        
+        if common_hobbies:
+            matching_user_details = await get_user_details(request=None, username=user['username'])
+            if matching_user_details:
+                for hobby in common_hobbies:
+                    found_match = f"A match found! {matching_user_details['full_name']} likes {hobby}! You can contact them here: {matching_user_details['phone_number']}, or at the email: {matching_user_details['email']}"
+                    matches.append(found_match)
+    
+    return JSONResponse(content=matches)
+
+
+@app.get('/{user_name}/find_people', response_class=HTMLResponse)
+async def find_people_form(request: Request, user_name: str):
+    matches_response = await find_shared_hobbies(user_name)
+    matches = matches_response.body.decode('utf-8')  # Decode the response body to a string
+    matches = json.loads(matches)  # Load the string into a list of matches
+    context = {'request': request, 'user_name': user_name, 'matches': matches}
+    return templates.TemplateResponse('find_people.html', context=context)
+
+
+@app.get('/about', response_class=HTMLResponse)
+async def about_page(request: Request):
+    context = {'request': request, 'user_name': None}
+    return templates.TemplateResponse('about.html', context)
+
+@app.get('/contact', response_class=HTMLResponse)
+async def contact_page(request: Request):
+    context = {'request': request, 'user_name': None}
+    return templates.TemplateResponse('contact.html', context)
+
 
 @app.get('/profile/{username}', response_class=HTMLResponse)
 async def profile_page(request: Request, username: str):
+    if username == 'None':
+        return RedirectResponse(url='/login')
     user = users_collection.find_one({"username": username})
     
     if user:
@@ -335,31 +449,10 @@ async def delete_user(request: Request, username: str):
 
 @app.get('/available_people', response_class=HTMLResponse)
 async def get_available_people(request: Request):
-    try:
-        available_people = list(users_collection.aggregate([
-            {
-                '$lookup': {
-                    'from': 'hobbies',
-                    'localField': '_id',
-                    'foreignField': 'user_id',
-                    'as': 'hobbies'
-                }
-            },
-            {
-                '$unwind': '$hobbies'
-            },
-            {
-                '$project': {
-                    'firstname': 1,
-                    'hobby': '$hobbies.hobby'
-                }
-            }
-        ]))
-        
-        templist = [f"{person['firstname']} enjoys {person['hobby']}" for person in available_people]
-        context = {'request': request, 'available_people': templist}
-        
-        return templates.TemplateResponse('available_people.html', context)
+    firstname_hobbies = await get_all_firstnames_and_hobbies() 
+    try:       
+        context = {'request': request, 'firstname_hobbies': firstname_hobbies, 'user_name' : None}  
+        return templates.TemplateResponse('available_people.html', context=context)
     
     except Exception as err:
         raise HTTPException(status_code=500, detail=f"Error retrieving available people: {err}")
